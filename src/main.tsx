c992701fb9fe43edd467dbc673278327a4bb5c4d
Cheikh Diop <cheikhtalibouyadiop6@gmail.com>
2026-02-06 19:24:48 +0000

    debug: add diagnostic logs to verify React mounting

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-)
+console.log('Main.tsx is starting...');
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+    console.error('Failed to find root element');
+} else {
+    console.log('Root element found, mounting React app...');
+    ReactDOM.createRoot(rootElement).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    )
+}
